d0150d8168bc34c0ae64c58d9f27e8612938b822
adamheinrich <adameksopot@gmail.com>
2025-01-10 12:31:48 +0100

    added offline storage for meals + adjustment of pull  to refresh + database is source of data for the list

--- app/src/main/java/com/schibsted/nde/database/MealEntity.kt
+++ app/src/main/java/com/schibsted/nde/database/MealEntity.kt
@@ -6,4 +6,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "meal")
 data class MealEntity(
     @PrimaryKey val id: String,
+    val strMeal: String,
+    val strCategory: String,
+    val strMealThumb: String,
+    val strYoutube: String?,
+    val strInstructions :String?,
 )
